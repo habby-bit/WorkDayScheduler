0022c77388050020c1fded78deb173fef5ef2053
Habby Olusesi <habbyolu@gmail.com>
2020-10-06 13:55:01 -0400

    created array for changing the colors of each time box

--- script.js
+++ script.js
@@ -1,43 +1,59 @@
 jQuery(document).ready(function() {
 
+    // Calling and displaying the current day and time
+
     var now = $("#currentDay") 
     now.text(moment().format("LLLL"));
-    console.log("now: ", moment().format("LLLL"))
 
     var textarea = $(".textarea")
+    var currentHour = parseInt(moment().format("H"));
 
-    var currentHour = moment().format('hA')
-    console.log('currentHour:', currentHour)
+    // Creating an array of every hour
 
-    var hours = $("span");
     var hoursArray = [];
-    
-    hours.each(function () {
-        hoursArray.push(this.innerHTML);
-    });
 
-    console.log('array:', hoursArray)
-
-    hoursArray.forEach(function(hour, index){
-        console.log('hour:', hour)
-
-        console.log('Comparison: ', moment().isAfter(hour))
-        console.log('moment: ', moment().format('hA'))
-
-        // if (currentHour.isSame(hour)) {
-        //     textarea.addClass("present");
-        // }
-        
-        // if (currentHour.isBefore(hour)) {
-        //     textarea.addClass("future");
-        // }
-
-        // if (currentHour.isAfter(hour)) {
-        //     textarea.addClass("past");
-        // }
+    $('span').each(function(){
+        hoursArray.push($(this).attr('id'))
     })
 
+    // Creating an array of every textarea
+
+    var textArray = [];
     
+    textarea.each(function(){
+        textArray.push($(this).attr('id'))
+    })
+    
+    // Looping through every textarea and hour to change class based on the time of day.
+
+    textArray.forEach(function(value, index) {
+
+        for (var i = 0; i < hoursArray.length; i++) {
+
+            // console.log('currentHour == hoursArray:', currentHour == hoursArray[i])
+            // console.log('currentHour > hoursArray:', currentHour > hoursArray[i])
+            // console.log('currentHour < hoursArray:', currentHour < hoursArray[i])
+
+            // console.log('currentHour:', currentHour)  
+            // console.log('hoursArray[i]:', hoursArray[i])
+
+            if (currentHour == hoursArray[i]) {   
+                $("#" + value).addClass("present");
+            }
+
+            if (currentHour > hoursArray[i]) {
+                $("#" + value).addClass("past");
+            }
+
+            if (currentHour < hoursArray[i]) {
+                $("#" + value).addClass("future");
+            }
+
+        }
+
+    });
+
+
     // How I would do this is I'd put an ID on the savebutton, like "save10". 
     // Then when a save button is clicked, use that ID, take the 10 off of it, 
     // and get the value out of the textarea using that number.  Then save the 
@@ -45,6 +61,7 @@ jQuery(document).ready(function() {
     // Then, when you go to render all the tasks, run through the possible numbers 
     // and get the value out of localstorage and put it into the texta area again.
 
+    // Saving the input of the user to local storage.
 
     var saveBtn = $(".saveBtn")
 
@@ -75,9 +92,6 @@ jQuery(document).ready(function() {
           storeEvents();          
       });
     })
-
-      
-
 
 
 });
